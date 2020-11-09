32acd2883cfa319861dacb0171e37799927159ac
sunyukun <sunyukun@cloudwalk.com>
2020-11-09 18:19:44 +0800

    add iphone pro max config

--- src/main/java/com/syk/store/alarm/constant/ItemEnum.java
+++ src/main/java/com/syk/store/alarm/constant/ItemEnum.java
@@ -11,7 +11,13 @@ public enum ItemEnum {
     ITEM5("MGLG3CH/A", "iPhone 12 Pro 256GB 金色"), ITEM6("MGLL3CH/A", "iPhone 12 Pro 512GB 金色"),
     ITEM7("MGLD3CH/A", "iPhone 12 Pro 128GB 海蓝色"), ITEM8("MGLH3CH/A", "iPhone 12 Pro 256GB 海蓝色"),
     ITEM9("MGLM3CH/A", "iPhone 12 Pro 512GB 海蓝色"), ITEM10("MGLA3CH/A", "iPhone 12 Pro 128GB 银色"),
-    ITEM11("MGLF3CH/A", "iPhone 12 Pro 256GB 银色"), ITEM12("MGLK3CH/A", "iPhone 12 Pro 512GB 银色");
+    ITEM11("MGLF3CH/A", "iPhone 12 Pro 256GB 银色"), ITEM12("MGLK3CH/A", "iPhone 12 Pro 512GB 银色"),
+    ITEM13("MGC53CH/A", "iPhone 12 Pro Max 256GB 银色"), ITEM14("MGC13CH/A", "iPhone 12 Pro Max 128GB 银色"),
+    ITEM15("MGCA3CH/A", "iPhone 12 Pro Max 512GB 银色"), ITEM16("MGC03CH/A", "iPhone 12 Pro Max 128GB 石墨色"),
+    ITEM17("MGC43CH/A", "iPhone 12 Pro Max 256GB 石墨色"), ITEM18("MGC93CH/A", "iPhone 12 Pro Max 512GB 石墨色"),
+    ITEM19("MGC23CH/A", "iPhone 12 Pro Max 128GB 金色"), ITEM20("MGC63CH/A", "iPhone 12 Pro Max 256GB 金色"),
+    ITEM21("MGCC3CH/A", "iPhone 12 Pro Max 512GB 金色"), ITEM22("MGC33CH/A", "iPhone 12 Pro Max 128GB 海蓝色"),
+    ITEM23("MGC73CH/A", "iPhone 12 Pro Max 256GB 海蓝色"), ITEM24("MGCE3CH/A", "iPhone 12 Pro Max 512GB 海蓝色");
 
     private final String code;
     private final String name;
